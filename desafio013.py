434ddc33db4f7dfbe90af86e6664b6e906ee9db0
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-08-23 20:56:19 -0300

    correção pequena

--- desafio013.py
+++ desafio013.py
@@ -1,4 +1,4 @@
 sal = float(input('Qual o salário do funcionário? R$'))
 novo = sal + (sal * 15 / 100)
-print(f'O novo salário do funcionário com 15% de aumento será de R${novo}')
+print(f'O novo salário do funcionário com 15% de aumento será de R${novo:.0f}')
 
