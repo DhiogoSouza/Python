afc35047c6e5f4fbe267c24f16c43464f3610679
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-08-24 19:00:49 -0300

    Programa que faz a conta de dias e km percorridos de um carro e no final o quanto deverá pagar.

---
+++ desafio015.py
@@ -0,0 +1,6 @@
+dias = int(input('Por quantos dias o carro foi alugado? '))
+km = float(input('Quantos km foram percorridos com o carro? '))
+preço = dias * 60 + km * 0.15
+print(f'O total a pagar será R${preço}')
+
+
