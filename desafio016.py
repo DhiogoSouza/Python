c1ba6bf43834c1dc8b2129744452fa748977457f
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-08-25 19:21:31 -0300

    Usando import math trunc para criar um programa que lê um valor e mostra sua porção inteira. Ex: 6.123 seria 6.

---
+++ desafio016.py
@@ -0,0 +1,11 @@
+from math import trunc
+valor = float(input('Digite um valor: '))
+pi = trunc(valor)
+print(f'O valor digitado foi {valor} e a sua porção inteira é {pi}')
+
+
+#Outra forma
+'''
+valor = float(input('Digite seu valor: '))
+print(f'O valor digitado foi {valor} e sua porção inteira é {int(valor)}')
+'''
