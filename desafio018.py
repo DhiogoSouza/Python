f047d38d3da2802c91e9d53507cf69aa509cd07f
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-08-25 19:54:25 -0300

    Programa que calcula seno, cosseno e tangente de um angulo

---
+++ desafio018.py
@@ -0,0 +1,6 @@
+from math import tan, cos, sin, radians
+a = float(input('Digite o ângulo: '))
+print(f'O ângulo de {a} tem o SENO de {sin(radians(a)):.2f}')
+print(f'O ângulo de {a} tem o COSSENO de {cos(radians(a)):.2f}')
+print(f'O ângulo de {a} tem a TANGENTE de {tan(radians(a)):.2f}')
+
