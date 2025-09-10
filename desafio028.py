95c6a25d54ba06040bcaab122b49f8ac8bfed181
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-09-10 10:45:26 -0300

    Mini-game. Programa que faz o computador "pensar" em um número inteiro entre 0 e 5 e pede para o usuário tentar descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.

---
+++ desafio028.py
@@ -0,0 +1,17 @@
+from random import randint
+from time import sleep
+
+print('='*100)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print('='*100)
+
+n = int(input('Em que número eu pensei? '))
+c = randint(0,5)
+print('PROCESSANDO...')
+sleep(1)
+if n == c:
+    print('Você venceu!!!')
+else:
+    print(f'VCOÊ PERDEU!!! Eu pensei no número {c}')
+
+
