abfd8867fe44b106ec68cd125fe4c44920738cda
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-09-10 10:24:54 -0300

    if and else

---
+++ aula10.py
@@ -0,0 +1,24 @@
+'''tempo = int(input('Quantos anos tem seu carro? '))
+if tempo <= 3:
+    print('Seu carro é novo.')
+else:
+    print('Seu carro é velho.')'''
+
+
+'''nome = str(input('Digite seu nome: '))
+if nome == 'Dhiogo':
+    print('Com H? que charme!')
+else:
+    print('Que nome sem graça!')
+print(f'Como vai, {nome}?')'''
+
+
+'''n1 = float(input('Qual foi a 1ª nota? '))
+n2 = float(input('Qual foi a 2ª nota? '))
+m = (n1 + n2) / 2
+if m >=9:
+    print(f' Sua nota final foi {m} Tirou onda!!!')
+elif m >=5 <9:
+    print(f' Sua nota final foi {m} Mandou bem!')
+else:
+    print(f' Sua nota final foi {m} Volta pra creche!')'''
