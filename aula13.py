da1fa1dd27babe00e9b3a5367645ca5409fa0aa0
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-09-20 10:35:36 -0300

    Nessa aula, vamos começar nossos estudos com os laços e vamos fazer primeiro o "for", que é uma estrutura versátil e simples de entender. Por exemplo:
    for c in range(0, 4):
         print(c)
    print('Acabou')

---
+++ aula13.py
@@ -0,0 +1,12 @@
+s = 0
+for c in range (0, 4):
+    n = int(input('Digite um numero: '))
+    s += n
+print(f'A soma entre os numeros foi {s}')
+
+
+'''for c in range (0, 4):
+    print(c)
+print('FIM')'''
+
+
