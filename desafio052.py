180a805f03604816f13647315b94e934a43890a2
DhiogoSouza <164440206+DhiogoSouza@users.noreply.github.com>
2025-09-23 09:05:51 -0300

    correções

--- desafio052.py
+++ desafio052.py
@@ -8,3 +8,7 @@ for c in range(1, num + 1):
         print('\033[31m', end=' ')
     print(f'{c}', end='')
 print(f'\n\033[m0 número {num} foi divisivel {tot} vezes')
+if tot == 2:
+    print('E por isso ele é PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
